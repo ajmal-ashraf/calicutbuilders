3f3361c9e338dbaa24475f0ab22e24d2e4a042af
Ajmal <ajmalasharaf001@gmail.com>
2023-03-06 08:38:13 +0530

    home page made

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
 import Header from './Components/Header/Header';
 import Hero from './Components/Hero/Hero';
-import { BrowserRouter, Routes} from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Routes>
           <Header/>
           <Hero/>
-        </Routes>
       </BrowserRouter>
     </div>
   );
